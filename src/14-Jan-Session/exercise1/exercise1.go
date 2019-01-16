7cfb8580b93d89a69a48a0d3c3069639f2162bdb
juanmassa <juan.massana5@gmail.com>
2019-01-16 19:43:54 +0100

    pequeños cambios

--- src/14-Jan-Session/exercise1/exercise1.go
+++ src/14-Jan-Session/exercise1/exercise1.go
@@ -7,37 +7,37 @@ import "bufio"
 import "libraries/newLines"
 
 func reader(port string, stopChannel chan bool){
-	fmt.Println("Listening on port ", port)
+	fmt.Println("READER - Listening on port", port)
 
 	// listen on all interfaces
 	listener, listenerError := net.Listen("tcp", ":" + port)
 
 	if listenerError != nil {
-		fmt.Println("Error while starting server\n", listenerError)
+		fmt.Println("READER - Error while starting server\n", listenerError)
 		os.Exit(1)
     }
 
 	connection, connectionError := listener.Accept()
 
 	if connectionError != nil {
-		fmt.Println("Error while accepting connection at server: \n", connectionError)
+		fmt.Println("READER - Error while accepting connection at server: \n", connectionError)
 	} else {
-		fmt.Println("Connection received at server")
+		fmt.Println("READER - Connection received at server")
 	}
 
 	for {
 		// will listen for message to process ending in newline (\n)
 		message, readError := bufio.NewReader(connection).ReadString('\n')
 		if readError != nil {
-		connection.Close()
-		break
+			connection.Close()
+			break
 		}
 
 		// output message received
-		fmt.Print("Message Received: ", string(message))	
+		fmt.Print("READER - Message Received: ", string(message))	
 
 		if "stop" == newLines.EraseNewLines(message) {
-			fmt.Println("Reader - Should stop") 
+			fmt.Println("READER - Should stop") 
 			connection.Close()
 			stopChannel <- true
 		}			
@@ -50,20 +50,22 @@ func writer(otherIP, otherPort string, stopChannel chan bool){
 	for connectionError != nil {
 		connection, connectionError = net.Dial("tcp", otherIP + ":" + otherPort)  
 	}		
-	fmt.Println("Succesful dial")	 
+	fmt.Println("WRITER - Succesful dial")	 
 
 	// run loop forever (or until ctrl-c)
 	for { 
 		// read in input from stdin
 		reader := bufio.NewReader(os.Stdin)    
-		fmt.Print("Text to send: ")
+		fmt.Print("WRITER - Text to send: ")
 		text, _ := reader.ReadString('\n')
 
-		// send to socket
-		fmt.Fprintf(connection, text + "\n")   
+		// send to socket if there is something to send
+		if(len(newLines.EraseNewLines(text)) > 0){
+			fmt.Fprintf(connection, text + "\n")
+		}		  
 
 		if "stop" == newLines.EraseNewLines(text) {
-			fmt.Println("Writer - Should stop")
+			fmt.Println("WRITER - Should stop")
 			stopChannel <- true
 			//TODO Aqui hay un caso en que si el mensaje a stopChannel tarda en llegar, vemos otra vez el 'Text to send' en consola
 		}
