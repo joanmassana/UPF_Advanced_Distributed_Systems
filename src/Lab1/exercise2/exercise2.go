ecbe615aa51408135b0707b31af93133ba4ec504
rcarlini-upf <roberto.carlini@upf.edu>
2019-01-19 15:00:42 +0100

    Code refactored in different functions, logging added and some issues fixed.

--- src/Lab1/exercise2/exercise2.go
+++ src/Lab1/exercise2/exercise2.go
@@ -4,106 +4,202 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"libraries/newLines"
 	"net"
 	"os"
-	"libraries/newLines"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
 )
 
+func receiveFile(baseDir, filename string, connection net.Conn) error {
 
-func reader(port string, stopChannel chan bool) {
-	fmt.Println("READER - Listening on port", port)
+	receiveFileError := os.MkdirAll(baseDir, os.FileMode(int(0775)))
+	if receiveFileError == nil {
+		log.Info("READER - Directories ready!")
 
-	// listen on all interfaces
-	listener, listenerError := net.Listen("tcp", ":" + port)
+		var destinationFile *os.File
+		destinationFile, receiveFileError = os.Create(baseDir + filename)
+		if receiveFileError == nil {
+			log.Info("READER - File ready!")
+			defer destinationFile.Close()
 
-	if listenerError != nil {
-		fmt.Println("READER - Error while starting server\n", listenerError)
-		os.Exit(1)
-	}
+			_, receiveFileError := io.Copy(destinationFile, connection)
+			if receiveFileError == nil {
+				log.Info("READER - Filename created at ", baseDir+filename)
 
-	connection, connectionError := listener.Accept()
+			} else {
+				log.Error("READER - Error receiving file: ", receiveFileError)
+			}
 
-	if connectionError != nil {
-		fmt.Println("READER - Error while accepting connection at server: \n", connectionError)
+		} else {
+			log.Error("READER - Error creating file: ", receiveFileError)
+		}
 	} else {
-		fmt.Println("READER - Connection received at server")
+		log.Error("READER - Error creating directories: ", receiveFileError)
 	}
+
+	return receiveFileError
+}
+
+func readerLoop(connection net.Conn, stopChannel chan bool) {
 
 	for {
+		text, readError := bufio.NewReader(connection).ReadString('\n')
+		if readError == nil {
 
-		incomingFileName, readError := bufio.NewReader(connection).ReadString('\n')
-		if readError != nil {
-			connection.Close()
-			break
+			text = newLines.EraseNewLines(text)
+			if text == "stop" {
+				log.Info("READER - Stop received! Shutting down...")
+				break
+
+			} else {
+				log.Info("READER - Filename received!")
+
+				baseDir := "src/Lab1/exercise2/files/received/"
+				receiveFileError := receiveFile(baseDir, text, connection)
+				if receiveFileError != nil {
+					log.Error("READER - Error receiving file: ", receiveFileError)
+				}
+			}
+
+		} else {
+			log.Error("READER - Error reading message: ", readError)
+			if readError == io.EOF {
+				log.Error("READER - EOF found, shuting down...")
+				break
+			}
 		}
-
-		destinationFile, fileCreationError := os.Create(incomingFileName)
-		if fileCreationError != nil {
-			fmt.Println("READER - Error creating file: ", fileCreationError)
-		}
-
-		_, fileReceptionError := io.Copy(destinationFile, connection)
-		if fileReceptionError != nil {
-			fmt.Println("READER - Error receiving file: ", fileReceptionError)
-		}
-
-		destinationFile.Close()
-
 	}
 }
 
-func writer(otherIP, otherPort string, stopChannel chan bool) {
-	// connect to this socket
-	connection, connectionError := net.Dial("tcp", otherIP+":"+otherPort)
-	for connectionError != nil {
-		connection, connectionError = net.Dial("tcp", otherIP+":"+otherPort)
+func reader(port string, stopChannel chan bool) {
+
+	log.Info("READER - Listening on port", port)
+
+	// listen on all interfaces
+	listener, listenerError := net.Listen("tcp", port)
+
+	if listenerError == nil {
+		connection, connectionError := listener.Accept()
+
+		if connectionError == nil {
+			log.Info("READER - Connection received at server")
+			defer connection.Close()
+
+			readerLoop(connection, stopChannel)
+			stopChannel <- true
+
+		} else {
+			log.Error("READER - Error while accepting connection at server: ", connectionError)
+		}
+
+	} else {
+		log.Error("READER - Error while starting server: ", listenerError)
 	}
-	fmt.Println("WRITER - Succesful dial")
+}
+
+func sendFile(baseDir, filename string, connection net.Conn) error {
+
+	//Open file to be sent
+	file, sendFileError := os.Open(baseDir + filename)
+	if sendFileError == nil {
+		defer file.Close()
+
+		//Send file details
+		var fileDetails os.FileInfo
+		fileDetails, sendFileError = file.Stat()
+		if sendFileError == nil {
+
+			_, sendFileError = fmt.Fprintf(connection, fileDetails.Name()+"\n")
+			if sendFileError == nil {
+
+				//Send file
+				_, sendFileError = io.Copy(connection, file)
+				// 	if sendFileError != nil {
+				// 		log.Error("WRITER - Error sending file: ", sendFileError)
+				// 	}
+
+				// } else {
+				// 	log.Error("WRITER - Error opening file: ", sendFileError)
+			}
+
+			// } else {
+			// 	log.Error("WRITER - Error retrieving file details: ", sendFileError)
+		}
+
+		// } else {
+		// 	log.Error("WRITER - Error opening file: ", sendFileError)
+	}
+
+	return sendFileError
+}
+
+func writerLoop(connection net.Conn, stopChannel chan bool) {
 
 	for {
 		// read in input from stdin
 		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("WRITER - Input full filename of file to send: ")
-		text, _ := reader.ReadString('\n')
 
-		if "stop" == newLines.EraseNewLines(text) {
-			fmt.Println("WRITER - Should stop")
-			fmt.Fprintf(connection, text + "\n")		//TODO Handle error
-			stopChannel <- true
+		fmt.Println("WRITER - Input full filename of file to send: ")
+		text, readError := reader.ReadString('\n')
+
+		if readError == nil {
+			text = newLines.EraseNewLines(text)
+			if "stop" == text {
+				log.Info("WRITER - Should stop")
+				fmt.Fprintf(connection, text+"\n") //TODO Handle error
+				break
+			} else if text == "" {
+				// do nothing
+
+			} else {
+				baseDir := "src/Lab1/exercise2/files/"
+				sendFileError := sendFile(baseDir, text, connection)
+				if sendFileError != nil {
+					log.Error("WRITER - Something went wrong while sending the file: ", sendFileError)
+					cwd, _ := os.Getwd()
+					log.Error("WRITER - CWD: ", cwd)
+				}
+			}
+		} else {
+			log.Error("WRITER - Error reading std input: ", readError)
 		}
-
-		//Open file to be sent
-		file, openFileError := os.Open("/files/test.txt")
-		if openFileError != nil {
-			fmt.Println("WRITER - Error opening file: ", openFileError)
-		}
-
-		//Send file details
-		fileDetails, fileDetailsError := file.Stat()
-		if fileDetailsError != nil {
-			fmt.Println("WRITER - Error retrieving file details: ", openFileError)
-		}
-
-		fmt.Fprintf(connection, fileDetails.Name() + "\n")		//TODO Handle error
-
-		//Send file
-		_, fileSendError := io.Copy(connection, file)
-		if openFileError != nil {
-			fmt.Println("WRITER - Error sending file: ", fileSendError)
-		}
-
-		file.Close()
 	}
+}
+
+func writer(otherIP, otherPort string, stopChannel chan bool) {
+
+	// connect to this socket
+	connection, connectionError := net.Dial("tcp", otherIP+otherPort)
+	for connectionError != nil {
+		connection, connectionError = net.Dial("tcp", otherIP+otherPort)
+	}
+	defer connection.Close()
+
+	log.Info("WRITER - Succesful dial")
+	writerLoop(connection, stopChannel)
+	stopChannel <- true
 }
 
 func main() {
 
-	argsWithoutProg := os.Args[1:]
-
-	thisPort := argsWithoutProg[0]
-	otherPort := argsWithoutProg[1]
-
 	otherIP := "127.0.0.1"
+	otherPort := ":6002"
+	thisPort := ":6001"
+	if len(os.Args) > 1 {
+		log.Info("Ports set by arguments...")
+
+		thisPort = os.Args[1]
+		if !strings.HasPrefix(":", thisPort) {
+			thisPort = ":" + thisPort
+		}
+
+		otherPort = os.Args[2]
+		if !strings.HasPrefix(":", otherPort) {
+			otherPort = ":" + otherPort
+		}
+	}
 
 	stopChannel := make(chan bool)
 
