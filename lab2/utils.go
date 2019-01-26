d50244d6408bd9e44e04dd5b610ed4b4de46d488
juanmassa <juan.massana5@gmail.com>
2019-01-26 12:31:54 +0100

    node and utils file added to lab2

---
+++ lab2/utils.go
@@ -0,0 +1,24 @@
+package lab2
+
+import "strings"
+
+// NormalizeNewLines normalizes \r\n (windows) and \r (mac)
+// into \n (unix)
+func NormalizeNewLines(inputText string) string {
+	// replace CR LF \r\n (windows) with LF \n (unix)
+	inputText = strings.Replace(inputText, "\r\n", "\n", -1)
+	// replace CF \r (mac) with LF \n (unix)
+	inputText = strings.Replace(inputText, "\r", "\n", -1)
+
+	return inputText
+}
+
+// EraseNewLines erases \r\n (windows) and \r (mac) and \n (unix)
+func EraseNewLines(inputText string) string {
+	// replace CR LF \n (windows / linux)
+	inputText = strings.Replace(inputText, "\n", "", -1)
+	// replace CF \r (mac)
+	inputText = strings.Replace(inputText, "\r", "", -1)
+
+	return inputText
+}
